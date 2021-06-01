91a0ea5749a0362c09ca5eb48fdeb7c1a4fa7f95
bikrajlama <bxl038@shsu.edu>
2021-06-01 12:43:53 -0500

    Add project files.

---
+++ LearningDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
