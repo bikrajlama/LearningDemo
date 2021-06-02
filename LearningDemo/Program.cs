ef45841d52fc520af3beffe24770e9993ece21c0
bikrajlama <bxl038@shsu.edu>
2021-06-02 10:59:36 -0500

    Removed line from megthod

--- LearningDemo/Program.cs
+++ LearningDemo/Program.cs
@@ -6,7 +6,7 @@ namespace LearningDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Hello World!");
         }
     }
 }
